869f7b7de7d6cac234c141b986cafbc87c68835e
sergiostebanpgx <sergiosteban700@gmail.com>
2024-05-20 16:35:03 -0500

    Initial gallery project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Component from './Component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! I'm Sergio Steban Parra
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Component />
-      </header>
+    <div className=" ">
+      <h1>React 18 Alpha</h1>
     </div>
   );
 }
