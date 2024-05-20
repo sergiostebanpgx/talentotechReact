13d8140fd3fa749cf5e72f66daca3fe78b915ca7
sergiostebanpgx <sergiosteban700@gmail.com>
2024-05-20 17:37:28 -0500

    Create Players.jsx

---
+++ src/components/Players.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Player from '../images/player01.jpg'
+
+const Players = () => {
+  return (
+    <div>
+        <img src={Player} alt="Jugadores de fútbol de talla mundial" />
+    </div>
+  )
+}
+
+export default Players
